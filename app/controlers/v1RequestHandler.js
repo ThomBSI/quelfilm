83a583fd41a44efb629210864972b3acbe0a25cc
Thomas MARQUIS <thomas@marquis.fr.cr>
2018-03-06 12:47:42 +0100

    06/03/18

--- app/controlers/v1RequestHandler.js
+++ app/controlers/v1RequestHandler.js
@@ -1,7 +1,19 @@
 /** Google Assistant helper library. */
 const DialogflowApp = require('actions-on-google').DialogflowApp;
+const AssistantApp = require('actions-on-google');
 const actionHandlers = require('./actionHandlers');
-const askEnum = require('./askEnum');
+const businessModule = require('../business/movies');
+const googleFormatter = require('../responseFormatter/googleFormatter');
+const actionNames = {
+    INPUT_MOVIES_UNGUIDED: 'input.movies.unguided',
+    INPUT_MOVIES_POPULAR: 'input.movies.popular',
+    INPUT_MOVIE_RECAP: 'input.movie.recap',
+    INPUT_WELCOME: 'input.welcome',
+    INPUT_UNKNOWN: 'input.unknown'
+};
+const app;
+/** Liste des noms d'action paramétrés dans la partie fullfilment des intents dans Dialogflow. */
+exports.actionNames = actionNames;
 
 /**
  * Function to handle v1 webhook requests from Dialogflow.
@@ -13,82 +25,17 @@ exports.processV1Request = function (request, response) {
     let parameters = request.body.result.parameters; // https://dialogflow.com/docs/actions-and-parameters
     let inputContexts = request.body.result.contexts; // https://dialogflow.com/docs/contexts
     let requestSource = (request.body.originalRequest) ? request.body.originalRequest.source : undefined;
-    const app = new DialogflowApp({
+    app = new DialogflowApp({
         request: request,
         response: response
     });
-    actionHandlers.actionHandlers(action)(parameters)
-        .then((response, askType) => {
-            switch (askType) {
-                case askEnum.ask:
-                    app.ask(response);
-                    break;
-                case askEnum.askForConfirmation:
-                    app.askForConfirmation(prompt);
-                    break;
-                case askEnum.askForDateTime:
-                    app.askForDateTime(prompt)
-                    break;
-                case askEnum.askForDeliveryAddress:
-                    app.askForDeliveryAddress()
-                    break;
-                case askEnum.askForNewSurface:
-                    app.askForNewSurface
-                    break;
-                case askEnum.askForPermission:
-                    app.askForPermission
-                    break;
-                case askEnum.askForPermissions:
-                    app.askForPermissions
-                    break;
-                case askEnum.askForSignIn:
-                    app.askForSignIn
-                    break;
-                case askEnum.askForTransactionDecision:
-                    app.askForTransactionDecision
-                    break;
-                case askEnum.askForTransactionRequirements:
-                    app.askForTransactionRequirements
-                    break;
-                case askEnum.askForUpdatePermission:
-                    app.askForUpdatePermission
-                    break;
-                case askEnum.askWithList:
-                    app.askWithList('Quelques idées...', response);
-                    break;
-                case askEnum.askWithCarousel:
-                    app.askWithCarousel
-                    break;
-                case askEnum.askToRegisterDailyUpdate:
-                    app.askToRegisterDailyUpdate
-                    break;
-            }
-        })
-        .catch((errorResponse) => {
-            app.ask(errorResponse);
-        });
+    actionHandlers(action)(parameters);
 }
 
-
-
-
-
-const { AssistantApp, DialogflowApp } = require('actions-on-google');
-const businessModule = require('../business/movies');
-const googleFormatter = require('../responseFormatter/googleFormatter');
-let actionNames = {
-    INPUT_MOVIES_UNGUIDED: 'input.movies.unguided',
-    INPUT_MOVIES_POPULAR: 'input.movies.popular',
-    INPUT_MOVIE_RECAP: 'input.movie.recap',
-    INPUT_WELCOME: 'input.welcome',
-    INPUT_UNKNOWN: 'input.unknown'
-};
-/** Liste des noms d'action paramétrés dans la partie fullfilment des intents dans Dialogflow. */
-exports.actionNames = actionNames;
 /** 
  * Appel le bon gestionnaire en fonction du nom de l'action passé en paramètre.
  */
-exports.actionHandlers = function (action) {
+const actionHandlers = function (action) {
     let functionHandler;
     switch (action) {
         case actionNames.INPUT_MOVIES_UNGUIDED:
@@ -114,66 +61,55 @@ exports.actionHandlers = function (action) {
 }
 
 function inputMoviesUnguidedHandler(parameters) {
-    return new Promise((resolve, reject) => {
-        let period;
-        if (!parameters.genres) parameters.genres = [];
-        if (!parameters.year) parameters.year = null;
-        if (!parameters.persons) parameters.persons = [];
-        if (!parameters.number) parameters.number = null;
-        businessModule.getMoviesByCriteria(
-            parameters.genres,
-            parameters.year,
-            parameters.period,
-            parameters.persons,
-            parameters.number)
-            .then((movieList) => {
-                let formatedResponse = googleFormatter.buildMoviesListItems(movieList);
-                resolve(formatedResponse);
-            })
-            .catch((errorMessage) => {
-                reject(googleFormatter.buildSimpleResponse(errorMessage.name));
-            });
-    });
+    let period;
+    if (!parameters.genres) parameters.genres = [];
+    if (!parameters.year) parameters.year = null;
+    if (!parameters.persons) parameters.persons = [];
+    if (!parameters.number) parameters.number = null;
+    businessModule.getMoviesByCriteria(
+        parameters.genres,
+        parameters.year,
+        parameters.period,
+        parameters.persons,
+        parameters.number)
+        .then((movieList) => {
+            let formatedResponse = googleFormatter.buildMoviesListItems(movieList);
+            resolve(formatedResponse);
+        })
+        .catch((errorMessage) => {
+            reject(googleFormatter.buildSimpleResponse(errorMessage.name));
+        });
 }
 
 function inputMoviesPopularHandler(parameters) {
-    return new Promise((resolve, reject) => {
-        businessModule.getBestMovies(parameters.number)
-            .then(moviesList => {
-                let formatedResponse = googleFormatter.buildMoviesListItems(moviesList);
-                resolve(formatedResponse);
-            })
-            .catch(errorMessage => {
-                reject(googleFormatter.buildSimpleResponse(errorMessage.name));
-            });
-    });
+    businessModule.getBestMovies(parameters.number)
+        .then(moviesList => {
+            let formatedResponse = googleFormatter.buildMoviesListItems(moviesList);
+            
+            app.askWithList('Quelques idées', formatedResponse);
+        })
+        .catch(errorMessage => {
+            reject(googleFormatter.buildSimpleResponse(errorMessage.name));
+        });
 }
 
 function inputMovieRecapHandler(parameters) {
-    return new Promise((resolve, reject) => {
-        businessModule.recapMovie(request.body.result.parameters['Movie'])
-            .then(recap => resolve(recap))
-            .catch(err => reject(err));
-    });
+    businessModule.recapMovie(request.body.result.parameters['Movie'])
+        .then(recap => resolve(recap))
+        .catch(err => reject(err));
 }
 
 /** The default welcome intent has been matched, welcome the user (https://dialogflow.com/docs/events#default_welcome_intent). */
 function inputWelcomeHandler(parameters) {
-    return new Promise((resolve, reject) => {
-        resolve('Hello, Welcome to my Dialogflow agent!');
-    });
+    resolve('Hello, Welcome to my Dialogflow agent!');
 }
 
 /** The default fallback intent has been matched, try to recover (https://dialogflow.com/docs/intents#fallback_intents). Use the Actions on Google lib to respond to Google requests; for other requests use JSON. */
 function inputUnknownHandler(parameters) {
-    return new Promise((resolve, reject) => {
-        resolve('Je rencontre un problème :-/ Pouvez-vous rééssayer plus tard ?');
-    });
+    resolve('Je rencontre un problème :-/ Pouvez-vous rééssayer plus tard ?');
 }
 
 /** Default handler for unknown or undefined actions. */
 function defaultHandler(parameters) {
-    return new Promise((resolve, reject) => {
-        resolve('Je ne comprend pas votre demande');
-    });
+    resolve('Je ne comprend pas votre demande');
 }
--- app/responseFormatter/googleFormatter.js
+++ app/responseFormatter/googleFormatter.js
@@ -2,16 +2,22 @@ const DialogflowApp = require('actions-on-google').DialogflowApp;
 
 /**
  * Construit une liste de films sous la forme d'une liste de cartes google assistant. 
+ * La liste en entrée doit comprendre entre 2 et 20 films (@Movie). 
+ * Lève une erreur si la liste d'entrée est plus grande que 20. 
+ * Retourne une simple carte si la liste ne contien qu'un élément. 
+ * Retourne un message simple si la liste est vide.
+ * 
  * @param {*} moviesList 
  */
 exports.buildMoviesListItems = function(moviesList) {
-    let listResponse = null;
-    let emptyMessage = {
-        speech: 'Aucun film n\'a pus être tourvé',
-        displayText: 'Aucun film n\'a pus être tourvé'
-    };
-    if(moviesList.length != 0) {
-        let app = new DialogflowApp();
+    let response = null;
+    let emptyMessage = exports.buildSimpleResponse('Aucun film n\'a pus être tourvé');
+    let app = new DialogflowApp();
+    if (moviesList.length > 20) {
+        throw new Error('La liste doit comprendre moins de 20 films');
+    } else if (moviesList.length === 1) {
+        response = buildrichResponseBasicCard(moviesList[0]);
+    } else if (moviesList.length != 0) {
         let listOptions = [];
         moviesList.forEach((movie) => {
             let item = buildSingleMovieOptionItem(movie);
@@ -20,14 +26,14 @@ exports.buildMoviesListItems = function(moviesList) {
             }
         });
         if(listOptions.length === 0) {
-            listResponse = emptyMessage;
+            response = emptyMessage;
         } else {
-            listResponse = app.buildList(`Les ${moviesList.length} films les plus populaires en ce moment`).addItems(listOptions);
+            response = app.buildList(`En voila ${moviesList.length}`).addItems(listOptions);
         }
     } else {
-        listResponse = emptyMessage;
+        response = emptyMessage;
     }
-    return listResponse;
+    return response;
 }
 
 /**
@@ -54,11 +60,28 @@ function buildSingleMovieOptionItem(movie) {
         let app = new DialogflowApp();
         item = app.buildOptionItem(`${movie.id}`)
             .setTitle(movie.title)
-            .setImage(movie.posterPath, movie.title);
-        let description = '';
-        if (movie.releaseDate != null) description = `${movie.releaseDate.getFullYear()}`;
-        if (movie.directorName != '') description = `${description} - de ${movie.directorName}`;
-        if (description != '') item.setDescription(description);
+            .setImage(movie.posterPath, movie.title)
+            .setDescription(buildMovieDescription(movie));
     }
     return item;
+}
+
+function buildrichResponseBasicCard(movie) {
+    let app = new DialogflowApp();
+    let basicCard = null;
+    if(movie && movie.title != '' && movie.id != '') {
+        basicCard = app.buildRichResponse()
+            .addBasicCard(app.buildBasicCard()
+                .setTitle(movie.title)
+                .setSubtitle(buildMovieDescription(movie))
+                .setImage(movie.posterPath, movie.title));
+    }
+    return basicCard;
+}
+
+function buildMovieDescription(movie) {
+    let description = '';
+    if (movie.releaseDate != null) description = `${movie.releaseDate.getFullYear()}`;
+    if (movie.directorName != '') description = `${description} - de ${movie.directorName}`;
+    return description;
 }
